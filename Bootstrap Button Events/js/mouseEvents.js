c290b94b64d4ac4f5b471e0f30cdfd20307873c5
sjaworowicz <125sjaworowicz@frhsd.com>
2024-01-22 10:58:22 -0500

    first commit

---
+++ Bootstrap Button Events/js/mouseEvents.js
@@ -0,0 +1,9 @@
+let btnOne = document.getElementById('click')
+btnOne.addEventListener("click", makeGreen);
+
+function makeGreen()
+{
+    // #a0b96b
+    console.log("#a0b96b");
+    btnOne.style.background="#a0b96b";
+}
