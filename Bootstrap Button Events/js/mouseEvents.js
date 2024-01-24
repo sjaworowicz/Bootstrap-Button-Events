5c12b4af82ae08c6da7853a92a50842efc6a9208
sjaworowicz <125sjaworowicz@frhsd.com>
2024-01-24 08:32:21 -0500

    Commit

--- Bootstrap Button Events/js/mouseEvents.js
+++ Bootstrap Button Events/js/mouseEvents.js
@@ -1,9 +1,19 @@
 let btnOne = document.getElementById('click')
-btnOne.addEventListener("click", makeGreen);
+btnOne.addEventListener("click", function(e){
+    e.target.style.background = "#a0b96b";
+});
 
-function makeGreen()
-{
-    // #a0b96b
-    console.log("#a0b96b");
-    btnOne.style.background="#a0b96b";
-}
+let btnTwo = document.getElementById('dblclick');
+btnTwo.addEventListener('dblclick', function(event){
+    event.target.style.border = "3px dotted #ec8f6a";
+});
+
+let btnThree = document.getElementById('mousedown')
+btnThree.addEventListener('mousedown', function(event){
+    event.target.style.border = "3px dotted blue";
+});
+
+let btnFour = document.getElementById('mouseenter')
+btnFour.addEventListener('mouseenter', function(event){
+     event.target.style.background = "red";
+})
